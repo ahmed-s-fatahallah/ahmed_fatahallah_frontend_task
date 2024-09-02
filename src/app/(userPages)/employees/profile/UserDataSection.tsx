b1dc5672f03ae49020ed7db02999b19627ec5c82
ahmed-fatahallah <ahmed.fathalla@nagwa.com>
2024-09-02 09:57:38 +0300

    refactor: get the paragraphs outside the form element

--- src/app/(userPages)/employees/profile/UserDataSection.tsx
+++ src/app/(userPages)/employees/profile/UserDataSection.tsx
@@ -19,66 +19,70 @@ export default function UserDataSection({
   const editingStyles = " border-solid border-black";
 
   return (
-    <form className="pe-[6px] grid grid-cols-2 gap-5">
-      <InputField
-        disabled={!isEditing}
-        variant="profile"
-        type="text"
-        name="firstName"
-        defaultValue={firstName}
-        wrapperClassName={isEditing ? editingStyles : ""}
-        key={isEditing}
-        autoFocus={!!isEditing}
-      >
-        First Name
-      </InputField>
-      <InputField
-        disabled={!isEditing}
-        variant="profile"
-        type="text"
-        name="lastName"
-        defaultValue={lastName}
-        wrapperClassName={isEditing ? editingStyles : ""}
-      >
-        Last Name
-      </InputField>
-      <InputField
-        disabled={!isEditing}
-        variant="profile"
-        type="tel"
-        name="mobileNumber"
-        defaultValue={phone}
-        wrapperClassName={isEditing ? editingStyles : ""}
-      >
-        Mobile Number
-      </InputField>
-      <InputField
-        disabled={!isEditing}
-        variant="profile"
-        type="email"
-        name="email"
-        defaultValue={email}
-        wrapperClassName={isEditing ? editingStyles : ""}
-      >
-        Email Address
-      </InputField>
-      <ProfileDataField title="Date of Birth" value="July 14, 1995" />
-      <ProfileDataField title="Marital Status" value="Single" />
-      <ProfileDataField title="Gender" value="Female" />
-      <ProfileDataField title="Nationality" value="Egypt" />
-      <ProfileDataField title="Address" value="Maadi" />
-      <ProfileDataField title="City" value="Cairo" />
-      <ProfileDataField title="State" value="Cairo" />
-      <ProfileDataField title="Zip Code" value="35624" />
-      <ProfileDataField title="Work's hours" value="180 hour" />
-      <div className="border-dark-gray-4 border-b-[1px] flex justify-between">
-        <ProfileDataField title="Salary/hour" value="300 EGP" />
-        <ProfileDataField
-          title="Total Salary"
-          value="54000 EGP"
-          className="text-light-red-2"
-        />
+    <div>
+      <form className="pe-[6px] grid grid-cols-2 gap-5 mb-5">
+        <InputField
+          disabled={!isEditing}
+          variant="profile"
+          type="text"
+          name="firstName"
+          defaultValue={firstName}
+          wrapperClassName={isEditing ? editingStyles : ""}
+          key={isEditing}
+          autoFocus={!!isEditing}
+        >
+          First Name
+        </InputField>
+        <InputField
+          disabled={!isEditing}
+          variant="profile"
+          type="text"
+          name="lastName"
+          defaultValue={lastName}
+          wrapperClassName={isEditing ? editingStyles : ""}
+        >
+          Last Name
+        </InputField>
+        <InputField
+          disabled={!isEditing}
+          variant="profile"
+          type="tel"
+          name="mobileNumber"
+          defaultValue={phone}
+          wrapperClassName={isEditing ? editingStyles : ""}
+        >
+          Mobile Number
+        </InputField>
+        <InputField
+          disabled={!isEditing}
+          variant="profile"
+          type="email"
+          name="email"
+          defaultValue={email}
+          wrapperClassName={isEditing ? editingStyles : ""}
+        >
+          Email Address
+        </InputField>
+      </form>
+      <div className="pe-[6px] grid grid-cols-2 gap-5">
+        <ProfileDataField title="Date of Birth" value="July 14, 1995" />
+        <ProfileDataField title="Marital Status" value="Single" />
+        <ProfileDataField title="Gender" value="Female" />
+        <ProfileDataField title="Nationality" value="Egypt" />
+        <ProfileDataField title="Address" value="Maadi" />
+        <ProfileDataField title="City" value="Cairo" />
+        <ProfileDataField title="State" value="Cairo" />
+        <ProfileDataField title="Zip Code" value="35624" />
+        <ProfileDataField title="Work's hours" value="180 hour" />
+        <div className="border-dark-gray-4 border-b-[1px] flex justify-between">
+          <ProfileDataField title="Salary/hour" value="300 EGP" />
+          <ProfileDataField
+            title="Total Salary"
+            value="54000 EGP"
+            className="text-light-red-2"
+          />
+        </div>
       </div>
-    </form>
+    </div>
   );
 }
