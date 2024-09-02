d9bce3527705e12f3f049d771c9fbaa174a7aac2
ahmed-fatahallah <ahmed.fathalla@nagwa.com>
2024-09-02 21:30:49 +0300

    feat: Add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import CypartaIcon from "@/assets/images/cypartal_logo.svg";
+import Button from "@/Components/Button";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <></>;
+  const router = useRouter();
+  return (
+    <section className="h-screen flex flex-col justify-center items-center gap-8">
+      <CypartaIcon className="mx-auto mb-8" />
+      <h1 className="text-center text-3xl font-bold">
+        Welcome to Ahmed Saeed&apos;s Cyparta Frontend Task
+      </h1>
+      <Button
+        variant="login"
+        onClick={() => {
+          router.push("/login");
+        }}
+      >
+        Go to Login Page
+      </Button>
+    </section>
+  );
 }
