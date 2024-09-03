510befcac1c01ea0d9053e98a08077675b44ac3c
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2024-09-03 03:04:21 +0300

    chore: Install vite svgr plugin and make custom plugin to handle svgr

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import * as path from "path";
+
+// Custom plugin for skipping svg imports
+const skipSvgImport = () => ({
+  name: "load-svg",
+  enforce: "pre" as const,
+  transform(_: string, id: string) {
+    if (id.endsWith(".svg")) {
+      return "export default () => {}";
+    }
+  },
+});
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr(), skipSvgImport()],
   test: {
     environment: "jsdom",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
